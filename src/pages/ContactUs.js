546e162e3d16dd7968c5b2b253f55e57246d5f08
Amrish-pathak <gg620093@gmail.com>
2025-05-14 17:45:17 +0530

    Add files via upload

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,6 +1,7 @@
 import ContactUsFrom from "../Components/ContactUsForm"
 import Footer from "../Components/Footer"
 import Header from "../Components/Header"
+import MapComponent from "../Components/MapComponent"
 
 
 
@@ -15,6 +16,7 @@ const ContactUs = () => {
                 <Header />
                 <section className="First-Section">
                     <ContactUsFrom />
+                    <MapComponent/>
                 </section>
                 <Footer />
             </div>
